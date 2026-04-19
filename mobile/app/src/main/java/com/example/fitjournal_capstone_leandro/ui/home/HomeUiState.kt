fa21533585416f9db9d4a0a0a304b53ab2975dc6
Leandro Ardiles <lardiles@protonmail.com>
2026-04-19 18:31:20 -0400

    removed mobile files to fix ignored

--- mobile/app/src/main/java/com/example/fitjournal_capstone_leandro/ui/home/HomeUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.fitjournal_capstone_leandro.ui.home
-
-sealed class HomeUiState {
-    object Loading : HomeUiState()
-    data class Success(val muscleGroups: List<String>) : HomeUiState()
-    data class Error(val message: String) : HomeUiState()
-    object Empty : HomeUiState()
-}
